f17980d9fdb21c04612fc8bae01ad986b926d5fd
lokeshkanagala <lokesh.kanagala@gmail.com>
2017-11-15 21:36:33 -0500

    Added SignUp functionality. Also added flash, express-session

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var ejs = require('ejs');
+var path = require('path');
 var engine = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
 
 var User = require('./models/user');
 
@@ -18,35 +22,28 @@ mongoose.connect('mongodb://root:root@ds045531.mlab.com:45531/ecommerce', functi
 });
 
 //Middleware
+app.use(express.static(path.join(__dirname+'/public')));
 app.use(morgan('dev'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "lokesh123**"
+}));
+app.use(flash());
+
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
-app.post('/create', function(req,res,next) {
-    var user = new User();
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
 
-    user.profile.name = req.body.name;
-    user.password = req.body.password;
-    user.email = req.body.email;
-
-    user.save(function(err) {
-        if(err) return next(err);
-
-        res.json('Successfully created a new user');
-    });
-
-});
+app.use(mainRoutes);
+app.use(userRoutes);
 
 
-app.get('/', function(req,res) {
-    res.render('main/home');
-});
-
-app.get('/about', function(req,res) {
-    res.render('main/about');
-})
 
 
 
